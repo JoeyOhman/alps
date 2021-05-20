5e9a3b7626d7d040cba85bae9bd242cdc06d3f8c
Joey Ohman <joey.ohman@arbetsformedlingen.se>
2021-05-20 13:18:15 +0200

    use transformers 2.4.1, use l2-norm, ignore loss on non-masked indices again

--- src/sample.py
+++ src/sample.py
@@ -125,7 +125,8 @@ def mask_tokens(inputs, tokenizer, args):
         padding_mask = labels.eq(tokenizer.pad_token_id)
         probability_matrix.masked_fill_(padding_mask, value=0.0)
     masked_indices = torch.bernoulli(probability_matrix).bool()
-    # labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+    labels[~masked_indices] = -100  # We only compute loss on masked tokens
     # tokenizer.pad_token_id
     # print("PAD TOKEN ID:", tokenizer.pad_token_id)
     # TODO: Removed only consider masked indices
